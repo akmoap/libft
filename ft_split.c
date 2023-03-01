08b7878c862f410fc6fe2d5511ac5315620938a3
mapter <mapter@student.s19.be>
2023-03-01 01:56:19 +0100

    split_finished

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mapter <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 17:54:57 by mapter            #+#    #+#             */
-/*   Updated: 2023/02/28 02:47:09 by mapter           ###   ########.fr       */
+/*   Updated: 2023/03/01 01:53:56 by mapter           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,20 @@
 int	count_words(char const *s, char c)
 {
 	int	count;
+	int	started;
 
-	count = 1;
+	count = 0;
+	started = 0;
 	while (*s)
 	{
-		if (*s == c && *s + 1 != c)
+		if (!started && *s != c && *s != '\0')
+			started = 1;
+		if (*s == c && *(s + 1) != c && *(s + 1) != '\0' && started)
 			count++;
 		s++;
 	}
+	if (started)
+		count++;
 	return (count);
 }
 
@@ -43,38 +49,28 @@ char	*get_word(char const *s, char c)
 	return (word);
 }
 
-int	get_position(char const *s, char c)
-{
-	int	position;
-
-	position = 0;
-	while (ft_strchr(s, c) - s == 1)
-		position++;
-	return (position);
-}
-
 char	**ft_split(char const *s, char c)
 {	
 	char	**list;
 	char	*word;
 	int		word_count;
 	int		i;
-	int		position;
 
 	word_count = count_words(s, c);
-	list = (char **) malloc(word_count * sizeof(char *));
+	list = (char **) malloc((word_count + 1) * sizeof(char *));
 	if (!list)
 		return (0);
 	i = 0;
 	word = "";
-	position = -1;
 	while (i < word_count)
 	{
-		position += ft_strlen(word);
-		position = get_position(s + position, c);
-		word = get_word(s + position, c);
+		while (*s == c)
+			s++;
+		word = get_word(s, c);
 		list[i] = word;
+		s = s + ft_strlen(word);
 		i++;
 	}
+	list[i] = '\0';
 	return (list);
 }
