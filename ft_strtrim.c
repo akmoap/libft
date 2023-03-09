39bcabf80fbd8a506c6b8afb642d537f90cd9ca8
Moishe Apter <mapter@fu-r4-p1.s19.be>
2023-03-09 14:31:24 +0100

    Behatslacha

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mapter <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 17:18:18 by mapter            #+#    #+#             */
-/*   Updated: 2023/03/08 17:39:31 by mapter           ###   ########.fr       */
+/*   Updated: 2023/03/09 14:24:21 by mapter           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,22 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (ft_strdup(s1));
 	start = 0;
 	end = ft_strlen(s1);
+	if (!end)
+		return (ft_strdup(""));
 	while (ft_strchr(set, s1[start]) && start <= end)
 		start++;
 	if (start > end)
 		return (ft_strdup(""));
 	while (ft_strchr(set, s1[end]) && end >= 0)
 		end--;
-	str = malloc(end - start + 2);
+	str = (char *) malloc(end - start + 2);
 	if (!str)
 		return (0);
 	ft_strlcpy(str, &s1[start], end - start + 2);
 	return (str);
 }
+/*
+void	main()
+{
+	ft_strtrim("", "");
+}*/
